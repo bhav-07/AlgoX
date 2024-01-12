18a3c714fd7b7fdb1b3c23a2741d9a766def7c3a
Omar Sulemani <omar.221ec230@nitk.edu.in>
2024-01-13 01:44:33 +0530

    API and UI routing

--- NextJs_Project/lib/characters.js
+++ NextJs_Project/lib/characters.js
@@ -1,5 +1,14 @@
 import { endpoint } from '@/utils/endpoint'
 
-export async function getAllCharacters() {}
+export async function getAllCharacters() {
+    const data = await fetch(`${endpoint}/characters`)
+  
+    if (!data.ok) {
+      throw new Error('Failed to fetch data')
+    }
+  
+    return data.json()
+  }
+  
 
 export async function getCharacterBySlug() {}
