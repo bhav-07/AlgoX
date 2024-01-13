6fbdd8852732a14134f24a61b8dc2ac172d4aecc
Hasi2005 <prathapahasitha@gmail.com>
2024-01-14 00:12:31 +0530

    Quiz section

--- next_js/app/quiz/[id]/page.jsx
+++ next_js/app/quiz/[id]/page.jsx
@@ -11,10 +11,25 @@ import { Container } from '@/components'
 import { Answer } from '@/components/Answer'
 import { endpoint } from '@/utils/endpoint'
 
+
+async function getQuizQuestion(id) {
+  const data = await fetch(`${endpoint}/quiz/${id}`)
+
+  if (!data.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return data.json()
+}
+
+
 export default async function Page({ params }) {
+  const { question } = await getQuizQuestion(params.id)
+
   return (
-    <main>
-      <h1>dynamic questions</h1>
-    </main>
+    <Container as="main" className="flex flex-col gap-5 py-5">
+      <h1 className="text-lg font-semibold">{question.title}</h1>
+      <Answer answers={question.answers} questionId={params.id} />
+    </Container>
   )
 }
