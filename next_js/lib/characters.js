837aaf5a2054a71855dddc0e2497c19c2acec8a6
Hasi2005 <prathapahasitha@gmail.com>
2024-01-13 23:49:39 +0530

    dynamic characters page

---
+++ next_js/lib/characters.js
@@ -0,0 +1,13 @@
+import { endpoint } from '@/utils/endpoint'
+
+export async function getAllCharacters() { 
+        const data = await fetch(`${endpoint}/characters`)
+      
+        if (!data.ok) {
+          throw new Error('Failed to fetch data')
+        }
+      
+        return data.json()
+      }
+
+export async function getCharacterBySlug() {}
